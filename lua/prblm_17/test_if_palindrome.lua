14cd03a7d9d628d91cfd481d22a3cd9ad37f438c
travisp <paul.travis@michigansugar.com>
2023-10-13 16:37:22 -0400

    completed problems 17 and 18

--- lua/prblm_17/test_if_palindrome.lua
+++ lua/prblm_17/test_if_palindrome.lua
@@ -3,13 +3,25 @@
 io.write("Provide a string to check if it is a palindrome: ")
 local str = io.read()
 local list = {}
+local is_palindrome = true
 
 for char in str:gmatch"." do
   table.insert(list, char)
 end
 
-for i = 0, #list, 1 do
+local list_size = #list
 
+for i = 1, #list, 1 do
+  io.write(list[i]..' '..list[list_size - (i - 1)]..'\n')
+  if list[i] ~= list[list_size - (i - 1)] then
+    is_palindrome = false 
+  end
+end
+
+if is_palindrome then
+  io.write(str..' is a palindrome.\n')
+else
+  io.write(str..' is not a palindrome.\n')
 end
 
 io.write('\n')
---
+++ lua/prblm_18/compute_sum_three_ways.lua
@@ -0,0 +1,36 @@
+#!/usr/bin/env lua
+
+package.path = package.path .. ';/home/travisp/Workspace/test/programming_exercises/lua/lib/?.lua'
+require 'lib'
+
+local list = generate_random_array(100, 0, 100)
+local for_sum = 0
+local while_sum = 0
+local recursive_sum = 0
+
+for i = 1, #list, 1 do
+  for_sum = for_sum + list[i]
+end
+
+local i = 1
+while (i < #list + 1) do
+  while_sum = while_sum + list[i]
+
+  i = i + 1
+end
+
+function looper(i, list)
+
+  if (i < #list) then
+    return list[i] + looper(i + 1, list)
+  else
+    return list[i]
+  end
+
+end
+
+recursive_sum = looper(1, list)
+
+io.write('for sum: '..for_sum..'\n')
+io.write('while sum: '..while_sum..'\n')
+io.write('recursive sum: '..recursive_sum..'\n')
